679c0a26e2956b8b286a7421f7d86bd5ffc186a7
tcho <tcho@cca.edu>
2020-08-24 22:51:22 -0700

    fixes prop statement in statelesscomponents --> renders prop1 sucessfully

--- src/components/stateless-components.js
+++ src/components/stateless-components.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function StatelessComponent(props) {
     return (
         <ul>
-            <li>{ props[0] }</li>
+            <li>{ props.props_1 }</li>
         </ul>
     )
 }
