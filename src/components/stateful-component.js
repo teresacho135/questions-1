a75505c5f15217e0c6d4b0eab49530661ddf599e
tcho <tcho@cca.edu>
2020-08-25 15:53:22 -0700

    creates two functions that will allow you to addnewItems to state and handle the change to display on our list

--- src/components/stateful-component.js
+++ src/components/stateful-component.js
@@ -6,13 +6,40 @@ class StatefulComponent extends React.Component {
         this.state={
             ...props
         }
+        this.addNewItemToState = this.addNewItemToState.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
+
+    handleChange(e) {
+        this.setState({
+            newItem: e.target.value
+        })
+    }
+
+    addNewItemToState() {
+        this.setState({
+            items: [...this.state.items, this.state.newItem],
+            newItem: '',
+        })
+    }
+
     render() {
         return (
-            <ul>
-                <h3>Healthy Green Smoothie Ingredients</h3>
-                    {this.state.items.map(item => <li>{ item }</li>)}
-            </ul>
+            <div>
+                <ul>
+                    <h3>Healthy Green Smoothie Ingredients</h3>
+                        {this.state.items.map(item => <li>{ item }</li>)}
+                </ul>
+                <input 
+                    type="text" 
+                    placeholder="Add an Ingredient Here"
+                    value={this.state.newItem}
+                    onChange={this.handleChange} 
+                />
+                <button
+                    onClick={this.addNewItemToState}
+                >Add Item to List</button>
+            </div>
         )
     }
 }
