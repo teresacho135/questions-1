3462a86f4016a029caadbbabb012062488c974ba
tcho <tcho@cca.edu>
2020-08-24 22:33:28 -0700

    creates a stateless component inside of App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import './App.css';
+import StatelessComponent from './components/stateless-components';
 
 function App() {
   return (
-    <div>Hello React</div>
+    <div>
+      <StatelessComponent />
+    </div>
   );
 }
 
