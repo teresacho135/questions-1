1ad7d0b52d298e6af28a6d028e39ff4ec59bf9fb
tcho <tcho@cca.edu>
2020-08-24 23:01:47 -0700

    displays all four props on the page

--- src/components/stateless-components.js
+++ src/components/stateless-components.js
@@ -4,6 +4,9 @@ export default function StatelessComponent(props) {
     return (
         <ul>
             <li>{ props.props_1 }</li>
+            <li>{ props.props_2 }</li>
+            <li>{ props.props_3 }</li>
+            <li>{ JSON.stringify(props.props_4) }</li>
         </ul>
     )
 }
