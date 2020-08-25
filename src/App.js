1dccd9e71bc624eeb2c7fea4b048d0f52c55faf2
tcho <tcho@cca.edu>
2020-08-24 23:01:11 -0700

    takes the prop3 and passes it as a string for it to be displayed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <StatelessComponent 
         props_1="This is prop 1"
         props_2={30 + 2}
-        props_3={false}
+        props_3={false.toString()}
         props_4={
           { key: "this is the key", value: "this is the value" }
         }
